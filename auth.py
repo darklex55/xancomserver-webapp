4c5b9a356cce6dd054102b8b9b371abc27f12cd7
Alex Karas <darklex55@gmail.com>
2023-08-22 17:48:53 +0300

    Password Forgot Process Implemented

--- auth.py
+++ auth.py
@@ -6,7 +6,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db, OFFICIAL_IP
 from flask_login import login_user, login_required, logout_user, current_user
 from datetime import datetime
-from website.python_utils import produceHashFromText, sendValidationEmail;
+from .python_utils import produceHashFromText, sendValidationEmail,  sendPasswordResetEmail;
+from random import random
 
 auth = Blueprint('auth', __name__)
 
@@ -114,3 +115,94 @@ def sign_up():
     resp = make_response(render_template("sign_up.html", boolean = session['username_check'], error_username=False))
 
     return resp
+
+@auth.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method=='GET':
+        session.clear()
+
+    if (current_user.is_authenticated):
+        return redirect(url_for('views.home'))
+
+    if 'username' not in session:
+        session['username_check'] = False
+
+    if (request.method=='POST'):
+        if session['username_check']:
+            email = request.form.get('e_mail')
+
+            user = User.query.filter_by(email=email).first()
+
+            if user:
+                user.auth_key = produceHashFromText(str(user.email) + str(user.password) + str(random()))
+                db.session.commit()
+                sendPasswordResetEmail(email, user.auth_key, OFFICIAL_IP)
+
+            session.clear()
+            flash('If the details provided were correct, check your email for the next steps (check Spam folder as well).', category='success')
+            return redirect(url_for('auth.login'))
+        
+        else:
+            print(request.form)
+            username = request.form.get('userName')
+
+            if (username):
+                user = User.query.filter_by(username=username).first()
+                
+                if (user):
+                    session['username'] = username
+                    session['username_check'] = True
+                else:
+                    flash('User does not exist', category='error')
+
+        
+    resp = make_response(render_template("forgot_password.html", boolean = session['username_check'], error_username=False))
+
+    return resp
+
+@auth.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    session.clear()
+    if request.method=='GET':
+        if 'auth_key' in request.args:
+            user = User.query.filter_by(auth_key=request.args['auth_key']).first()
+
+            if user:
+                return make_response(render_template("reset_password.html"))
+            else:
+                flash('Invalid Resource', category='error')
+        else:
+            flash('Invalid Resource', category='error')
+
+        return redirect(url_for('views.home'))
+    
+    else:
+        if 'auth_key' in request.args:
+            user = User.query.filter_by(auth_key=request.args['auth_key']).first()
+            if user:
+                password1 = request.form.get('password1')
+                password2 = request.form.get('password2')
+                email = request.form.get('e_mail')
+
+                if password1 != password2:
+                    flash('Passwords don\'t match.', category='error')
+                    return make_response(render_template("reset_password.html"))
+                elif len(password1) < 7:
+                    flash('Password must be at least 7 characters.', category='error')
+                    return make_response(render_template("reset_password.html"))
+                elif check_password_hash(user.password, password1):
+                    flash('You cannot use the same password as you had before. Please change your password.', category='error')
+                    return make_response(render_template("reset_password.html"))
+                else:
+                    user.password=generate_password_hash(password1, method='sha256')
+                    user.auth_key = produceHashFromText(str(user.email) + str(user.password) + str(random()))
+                    db.session.commit()
+                    flash("New password set successfuly!", category='success')
+                    return redirect(url_for('views.home'))
+
+            else:
+                flash('Invalid Resource', category='error')
+        else:
+            flash('Invalid Resource', category='error')
+        
+        return redirect(url_for('views.home'))
--- python_utils.py
+++ python_utils.py
@@ -179,9 +179,23 @@ def getAllUserData():
 
 def sendValidationEmail(email, auth_key, url_root):
     msg = MIMEMultipart()
-    msg['Subject'] = 'HelloString Account Verification'
+    msg['Subject'] = 'Xancomserver Account Verification'
     msg['From'] = 'darklex55server@gmail.com'
     text = 'Please validate your account by clicking the following link: '+ url_root +'/verification?auth_key='+ auth_key
+    msg.attach(MIMEText(text,'plain'))
+    smtp = smtplib.SMTP('smtp.gmail.com:587')
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login('darklex55server@gmail.com','qpvfntgdvddadhqo')
+    smtp.sendmail('darklex55server@gmail.com',email,msg.as_string())
+    smtp.quit()
+
+def sendPasswordResetEmail(email, auth_key, url_root):
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Xancomserver Account Password Reset'
+    msg['From'] = 'darklex55server@gmail.com'
+    text = 'If you did not request for a new password, you can ignore this email. You can reset your password by following this link: http://'+ url_root +'/reset_password?auth_key='+ auth_key
     msg.attach(MIMEText(text,'plain'))
     smtp = smtplib.SMTP('smtp.gmail.com:587')
     smtp.ehlo()
